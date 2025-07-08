397369350ce63f5a1c54b09977bf4fc5856c252f
tomrcc <tom@cloudcannon.com>
2025-07-08 14:44:22 +0800

    Get rid of iframe temporarily, make it easier to see snippet

--- components/snippets/button.jsx
+++ components/snippets/button.jsx
@@ -1,5 +1,7 @@
 export default function ButtonSnippet({ button_text, button_link }) {
 	return (
-    <a href={button_link}>{button_text}</a>
+		<div style={{backgroundColor: 'red'}}>
+			<a href={button_link}>{button_text}</a>
+		</div>
 	);
 }
