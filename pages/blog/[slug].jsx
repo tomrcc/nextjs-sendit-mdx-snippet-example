2207adb102e11c9b738e7d70c25a5d7a03e7eff7
tomrcc <tom@cloudcannon.com>
2025-07-08 19:15:14 +0800

    Testing importing all components

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -22,6 +22,10 @@ export default function Post({ page, posts, mdxSource, dateFormatted }) {
 	const wordCount = page.content.split(" ").length;
 	const readingTime = Math.floor(wordCount / 183);
 	console.log("allComponentsClient:", allComponents)
+	for (const component of allComponents) {
+		console.log("componentClient:", component)
+		console.log("componentClientDefault:", component.default)
+	}
 
 	return (
 		<DefaultLayout page={page}>
